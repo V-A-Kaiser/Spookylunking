b200c122e53a545476fa712ac8e73947c74fa26b
Valerie Kaiser <github@vakaiser.com>
2025-10-24 16:57:57 -0700

    Added a background video and title.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,23 @@
 <script lang="ts">
   import '../app.css';
-  import favicon from '$lib/assets/favicon.svg';
+  import favicon from '$lib/assets/icon.png';
+  import video from '$lib/assets/websitebackground.webm';
 
   let { children } = $props();
 </script>
 
 <svelte:head>
   <link rel="icon" href={favicon} />
+  <title>Spookylunking</title>
 </svelte:head>
 
-{@render children?.()}
+<!-- Background Video -->
+<video src={video} autoplay muted loop playsinline disablepictureinpicture disableremoteplayback class="absolute inset-0 z-[-1] w-full h-full object-cover"/>
+
+<header class="flex justify-center items-center p-4">
+  <h1 class="font-cormorant leading-none text-[8vw] md:text-6xl font-thin uppercase text-white text-shadow-lg/90 tracking-wide">Spookylunking</h1>
+</header>
+
+<main>
+  {@render children?.()}
+</main>
--- src/routes/+page.svelte
+++
@@ -1,1 +0,0 @@
-<h1>Spookylunking</h1>
